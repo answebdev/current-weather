cd653c4ae0c1339ef6461c352a963c57d1366707
ANS <36783010+answebdev@users.noreply.github.com>
2021-03-31 15:14:39 -0700

    add shell components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import WeatherUI from './WeatherUI';
 
-function App() {
+const App = () => {
+  const [weather, setWeather] = useState([]);
+  const [search, setSearch] = useState({
+    city: '',
+    country: '',
+  });
+
+  const APIKEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+  async function fetchWeather(e) {
+    e.preventDefault();
+    if (search.city === '') {
+      console.log('Add values');
+    } else {
+      const data = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${search.city},${search.country}&APPID=${APIKEY}`
+      )
+        .then((res) => res.json())
+        .then((data) => data);
+
+      setWeather({ data: data });
+      console.log(data);
+    }
+  }
+
+  const handleChange = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    if (name === 'city') {
+      setSearch({ ...search, city: value });
+    }
+    if (name === 'country') {
+      setSearch({ ...search, country: value });
+    }
+  };
+
+  // ALERNATIVE
+  // const [weather, setWeather] = useState([]);
+  // const [search, setSearch] = useState({
+  //   city: '',
+  //   country: '',
+  // });
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(false);
+
+  // const APIKEY = process.env.REACT_APP_WEATHER_API_KEY;
+  // const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${search.city},${search.country}&APPID=${APIKEY}`;
+
+  // const fetchWeather = (e) => {
+  //   e.preventDefault();
+  //   setIsLoading(true);
+  //   setError(null);
+
+  //   fetch(apiURL)
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setIsLoading(false);
+  //       setWeather({ data: data });
+  //       console.log(data);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error:', error);
+  //       setError(error);
+  //       setIsLoading(false);
+  //     });
+  // };
+
+  // const handleChange = (e) => {
+  //   let name = e.target.name;
+  //   let value = e.target.value;
+
+  //   if (name === 'city') {
+  //     setSearch({ ...search, city: value });
+  //   }
+  //   if (name === 'country') {
+  //     setSearch({ ...search, country: value });
+  //   }
+  // };
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Weather App
-        </a>
-      </header>
+    <div>
+      <span>Weather</span>
+      <br />
+      <form>
+        <input
+          type='text'
+          placeholder='City'
+          name='city'
+          onChange={(e) => handleChange(e)}
+        />
+        &nbsp; &nbsp; &nbsp;&nbsp;
+        <input
+          type='text'
+          placeholder='Country'
+          name='country'
+          onChange={(e) => handleChange(e)}
+        />
+        <button onClick={(e) => fetchWeather(e)}>Submit</button>
+      </form>
+
+      {/* {!isLoading ? <div className='text-center'></div> : <Spinner />}
+        {!isLoading && error ? (
+          <div>
+            <p style={{ textAlign: 'center' }}>Oh, no. something went wrong!</p>
+          </div>
+        ) : null} */}
+
+      {weather.data !== undefined ? (
+        <div>
+          <WeatherUI data={weather.data} />
+        </div>
+      ) : null}
     </div>
   );
-}
+};
 
 export default App;
